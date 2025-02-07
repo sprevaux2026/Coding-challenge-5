43e4df5858df95f2da34c30e5b13aae69453e26f
sprevaux2026 <stevenprevaux@gmail.com>
2025-02-07 12:41:25 -0500

    Task 1 - Customer Profile

---
+++ cc_5.js
@@ -0,0 +1,11 @@
+// Task 1: Object Properties - Customer Profile
+let customer = {
+    name: "Johnny Test",
+    age: 15,
+    email: "JohhnyTest@gmail.com"
+};
+
+// Log each property from above
+console.log(`Customer Name: ${customer.name}`);
+console.log(`Customer Age: ${customer.age}`);
+console.log(`Customer Email: ${customer.email}`);
