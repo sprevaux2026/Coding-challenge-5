c99c7fac77735845258cc52cc7e71ff4cef46d21
sprevaux2026 <stevenprevaux@gmail.com>
2025-02-07 12:43:55 -0500

    Task 2 - Order Details

--- cc_5.js
+++ cc_5.js
@@ -9,3 +9,16 @@ let customer = {
 console.log(`Customer Name: ${customer.name}`);
 console.log(`Customer Age: ${customer.age}`);
 console.log(`Customer Email: ${customer.email}`);
+
+// Task 2: Object Methods - Order Details
+let order = {
+    orderId: 99999,
+    totalAmount: 99,
+    status: "Processing",
+    displayOrder: function() {
+        console.log(`Order ID: ${this.orderId}, Total Amount: $${this.totalAmount}, Status: ${this.status}`); //logged the info
+    }
+};
+
+// Call the method to display order details
+order.displayOrder();
